1f98ec4ca37f095db70d687839b38f3ac5e323d8
David gp <juliograjalesparedes127@gmail.com>
2022-11-20 21:14:21 +0100

    se añadieron nuevos componentes para la peteciones hacia la api y otro para mostrar las imagenes en cards

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,33 +1,43 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-    const [categories, setCategories] = useState(['One Punch' , 'God of war']);
+    const [categories, setCategories] = useState(['One Punch']);
 
     const onAddCategory = (newCategory) =>{
-        // console.log(newCategory)
-        setCategories(cat => [newCategory ,...cat])
+        
+        if(categories.includes(newCategory)) return;
+
+        setCategories(cat => [newCategory ,...cat]);
         
     }
 
     return (
         <>
             {/* Titulo */}
+
             <h1>GifExpertApp</h1>
 
             {/* Input */}
+
             <AddCategory 
-                // setCategories={ setCategories }
                 onNewCategory = {(value) => onAddCategory(value) }
             />
             
 
             {/* Listado de Gifs */}
-            <ol>
-                { categories.map(category => <li key={ category }>{ category }</li>) }
-            </ol>
 
-                {/* Gif item */}
+            { 
+                categories.map(category => 
+                    (
+                        <GifGrid key={ category } category={ category }/>
+                    )
+                )
+            
+            }
+            
+
         </>
     )
 }
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,13 @@
+export const getGifs = async ( categoty ) =>{
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=GY39uAIJ9HaU30gpr4cq4tYRdQc9EiOo&q=${ categoty }&limit=10`;
+    const res = await fetch(url);
+    const { data } = await res.json();
+
+    const gif = data.map( img => ({
+        id : img.id,
+        title : img.title,
+        url : img.images.downsized_medium.url
+
+    }))
+    return(gif)
+}
